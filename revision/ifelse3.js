2f11bffa6abaf7177e32c1e4cbe3ad921e30d2da
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-16 03:42:45 +0000

    16

---
+++ revision/ifelse3.js
@@ -0,0 +1,58 @@
+//if else
+// passing or failling an exam
+// let marks=45;
+// if (marks>=50){
+//     console.log("passed this exam");
+    
+// }
+// else {
+//     console.log("you failed this exam");
+    
+// }
+
+
+//traffic light
+// let color="red"
+// if (color=="green"){
+//     console.log("yu may go");
+    
+// } else if (color=="yellow"){
+//     console.log("get ready to go");
+
+//when 2 as input compare with string gives true
+//when 2 as input compared strictly output
+// let color=2
+// if (color==="2"){
+//     console.log("yu may go");
+    
+// } else if (color=="yellow"){
+//     console.log("get ready to go");
+    
+// }
+
+// else {
+//     console.log("stop");
+    
+// }
+
+
+
+// file existence
+// let fileexist=""    //false
+// if (fileexist){
+//     console.log("success");
+    
+// } else {
+//     console.log("sorry");
+    
+// }
+
+
+// let fileexist="0" //true
+// if (fileexist){
+//     console.log("success");
+    
+// } else {
+//     console.log("sorry");
+    
+// }
